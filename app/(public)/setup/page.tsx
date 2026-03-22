d291d9bae96437678073399c389a809de13c1b3d
Joey Yax <joey@joeyyax.com>
2026-03-21 19:40:04 -0700

    fix(build): force-dynamic on pages that query DB at render time

--- app/(public)/setup/page.tsx
+++ app/(public)/setup/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import { needsSetup } from "@/lib/setup";
 import { SetupWizard } from "./setup-wizard";
 
+export const dynamic = "force-dynamic";
+
 export default async function SetupPage() {
   if (!(await needsSetup())) {
     redirect("/");
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import { getSession } from "@/lib/auth/session";
 import { needsSetup } from "@/lib/setup";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   if (await needsSetup()) {
     redirect("/setup");
